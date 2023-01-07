b3344730e1b2729f11ea39a0672fb9934bbf2fd1
Diogo Moura <diogoodev@gmail.com>
2023-01-07 13:23:02 -0300

    Estudos do dia 07-01-2023

--- push.bat
+++ push.bat
@@ -1,20 +1,25 @@
 @echo off
 
-rem Obtém a data atual no formato YYYY-MM-DD
+rem Muda para o diretório "D:\Diogo\Estudos-Alura-ONE"
+cd /d "D:\Diogo\Estudos-Alura-ONE"
+
+rem Adiciona todos os arquivos na área de stage
+git add .
+
+rem Obtém a data atual do sistema
 for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
 set "YY=%dt:~2,2%"
 set "YYYY=%dt:~0,4%"
 set "MM=%dt:~4,2%"
 set "DD=%dt:~6,2%"
-set "data=%YYYY%-%MM%-%DD%"
+set "HH=%dt:~8,2%"
+set "Min=%dt:~10,2%"
+set "Sec=%dt:~12,2%"
 
-rem Adiciona todas as alterações ao índice do Git
-git add .
+set "data=%DD%-%MM%-%YYYY%"
 
-rem Faz o commit com a mensagem "Estudos do dia YYYY-MM-DD"
+rem Faz o commit com a mensagem "Estudos do dia $data"
 git commit -m "Estudos do dia %data%"
 
-rem Envia os commits para o repositório remoto
-git push
-
-echo Commit e push realizados com sucesso!
+rem Faz o push para o repositório no GitHub
+git push origin master
